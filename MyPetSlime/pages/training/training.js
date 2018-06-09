2f2a1b12f9596a15d569755c0edd8f20dab74cde
Hecheng Li <lihecheng93@gmail.com>
2018-06-09 09:11:41 +0800

    主页面

--- MyPetSlime/pages/training/training.js
+++ MyPetSlime/pages/training/training.js
@@ -1,5 +1,6 @@
 // pages/training/training.js
 Page({
+<<<<<<< Updated upstream
   data: {
     trainFlag:false
   },
@@ -9,5 +10,69 @@ Page({
     this.setData ({
       trainFlag: e.detail.value
     })
+=======
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+>>>>>>> Stashed changes
   }
 })
